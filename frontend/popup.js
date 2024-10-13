1706d569119f010744d1c407640bb8bb63c13af5
Rensildi <rensildi.kalanxhi1@gmail.com>
2024-10-13 03:15:02 -0500

    php files

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,3 +1,3 @@
 document.getElementById('openWebsite').addEventListener('click', () => {
-    chrome.tabs.create({ url: 'https://your-website.com' });
+    chrome.tabs.create({ url: 'http://127.0.0.1:5500/frontend/index.html' });
 });
