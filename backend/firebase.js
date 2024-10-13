6c375abe93b474dcc0e06a7d44366cd991261254
Rensildi <rensildi.kalanxhi1@gmail.com>
2024-10-12 23:12:33 -0500

    Firebase

---
+++ backend/firebase.js
@@ -0,0 +1,10 @@
+// backend/firebase.js
+const admin = require('firebase-admin');
+const serviceAccount = require('./path/to/your/serviceAccountKey.json'); // Replace with your path
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://<your-database-name>.firebaseio.com" // Replace with your Firebase database URL
+});
+
+module.exports = admin;
